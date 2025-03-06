a97f55c4911814cbc55a6cf52b4a30804d2383a5
yurign000 <yurign000@gmail.com>
2025-03-06 11:14:11 -0300

    feat: add a function to get an array with random words

--- script.js
+++ script.js
@@ -5,3 +5,15 @@ var typo_area = document.querySelector("#typo__area");
 function sameTextAreaCheck(){
     return placeholder.value.startsWith(textarea.value);
 }
+
+async function generateRandomWords(quantity){
+    let random_words = [];
+
+    await fetch(`https://random-word-api.vercel.app/api?words=${quantity}`).then(async data => {
+        let words = await data.json();
+    
+        random_words = words;
+    })
+
+    return random_words;
+}
