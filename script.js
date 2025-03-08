acd825604389902b7515820416160e9e16534b97
yurign000 <yurign000@gmail.com>
2025-03-08 10:30:07 -0300

    feat: add typo warning

--- script.js
+++ script.js
@@ -24,14 +24,14 @@ function inputOnTypoArea(isSame, key){
     let letter = document.createElement('span');
 
     if(key.inputType == 'deleteContentBackward')
-        typo_area.lastChild.remove()
+        typo_area.lastChild.remove();
     else{
-        letter.innerHTML = key.data
+        letter.innerHTML = key.data;
 
         if(isSame){
             letter.style.backgroundColor = "transparent";
-            letter.style.color = "transparent"
-            letter.style.outline = "none"
+            letter.style.color = "transparent";
+            letter.style.outline = "none";
         } 
             
         typo_area.append(letter);
