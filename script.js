3eae71efb1939d578af7f8c74ac1a050aa39a485
yurign000 <yurign000@gmail.com>
2025-03-12 13:51:05 -0300

    feat: placeholder

--- script.js
+++ script.js
@@ -1,26 +1,27 @@
-var wordsGenerated = '';
-var typingTimer = 5;
-var isTyping = false;
-var wordsQuantity = 40;
+var words_generated = '';
+var typing_area = document.getElementById('typing__area');
+var typing_timer = 5;
+var is_typing = false;
+var words_quantity = 40;
 
-var invalidKeysPressed = {
+var invalid_keys_pressed = {
     'Control': false,
     'Tab': false,
 }
-document.addEventListener('keydown', ({key}) => invalidKeysPressed[key] = true)
-document.addEventListener('keyup', ({key}) => invalidKeysPressed[key] = false)
+document.addEventListener('keydown', ({key}) => invalid_keys_pressed[key] = true)
+document.addEventListener('keyup', ({key}) => invalid_keys_pressed[key] = false)
 
 window.addEventListener('load', async () => {
     // wordsGenerated = (await generateRandomWords(wordsQuantity)).join().replace(/,/g, ' ');
 })
 
 function blockSpecialKeys(event){
-    if(invalidKeysPressed['Tab'] && event.key == 'Enter'){
+    if(invalid_keys_pressed['Tab'] && event.key == 'Enter'){
         event.preventDefault()
         window.location.reload()
     }
     else if(
-        invalidKeysPressed['Control'] && event.key == 'Backspace' ||
+        invalid_keys_pressed['Control'] && event.key == 'Backspace' ||
         ['Enter','Tab','Alt'].includes(event.key)
     ){
         event.preventDefault()
@@ -41,8 +42,39 @@ async function generateRandomWords(quantity){
 
 function startTimer(){
     let interval = setInterval(() => {
-        if(typingTimer-- <= 1){ 
+        if(typing_timer-- <= 1){ 
             clearInterval(interval);
         }
     }, 1000);
+}
+
+//test
+let sentence = [
+    'abacaxi', 'laranja', 'maça', 'pera', 'tomate', 
+    'uva', 'tomada', 'coração','abacaxi', 'laranja', 
+    'maça', 'pera', 'tomate', 'uva', 'tomada', 'coração',
+    'maça', 'pera', 'tomate', 'uva', 'tomada', 'coração',
+    'maça', 'pera', 'tomate', 'uva', 'tomada', 'coração',
+    'abacaxi', 'laranja', 'maça', 'pera', 'tomate', 'uva', 'tomada', 
+    'coração'
+];
+
+makePlaceholder()
+function makePlaceholder(){
+    let letterCount = 0;
+
+    sentence.forEach((word) => {
+        let word_element = document.createElement('label');
+        word_element.setAttribute('class','words');
+        
+        for(let i = 0; i <= word.length; i++, letterCount++){
+            let letter_element = document.createElement('span');
+            letter_element.setAttribute('class','letters');
+            letter_element.setAttribute('id','letter'+letterCount);
+            letter_element.innerHTML = word[i] || ' '; 
+
+            word_element.append(letter_element);
+        }
+        typing_area.append(word_element);
+    })
 }
